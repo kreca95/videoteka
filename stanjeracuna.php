ad7d6af24ce6358b0a0713c2b0b8bf6b18382a3c
Kresimir Sutalo <kresimir095@gmail.com>
2020-02-27 15:55:40 +0100

    ispravke oko sintakse2

--- stanjeracuna.php
+++ stanjeracuna.php
@@ -22,7 +22,7 @@
     </div>
     <div class="form-group">
       <label for="">Kolicina novca:</label>
-      <input type="number" class="form-control" name="novac">
+      <input type="number" class="form-control" name="novac" min="1">
     </div>
 
     <button type="submit" class="btn btn-info">Uplata</button>
